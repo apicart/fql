f7244ef615a5d55258095ece17b6ecd7f0514820
Tomas Pilar <email@tomaspilar.cz>
2025-09-14 10:20:40 +0200

    Range. add helper methods

--- src/Token/Token/Range.php
+++ src/Token/Token/Range.php
@@ -2,9 +2,10 @@
 
 namespace Apicart\FQL\Token\Token;
 
-use Apicart\FQL\Tokenizer\Tokenizer;
+use DateTimeImmutable;
 use Apicart\FQL\Value\Token;
 use InvalidArgumentException;
+use Apicart\FQL\Tokenizer\Tokenizer;
 
 final class Range extends Token
 {
@@ -12,6 +13,14 @@ final class Range extends Token
     public const TYPE_INCLUSIVE = 'inclusive';
 
     public const TYPE_EXCLUSIVE = 'exclusive';
+
+    public const DATE_FORMAT = 'Y-m-d';
+    public const DATE_REGEX = '/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/';
+
+    public const DATETIME_FORMAT = 'Y-m-d\TH:i:s\Z';
+    public const DATETIME_REGEX = '/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])T([01]\d|2[0-3]):([0-5]\d):([0-5]\d)(\.\d{1,9})?Z$/';
+    public const RELATIVE_DATE_SEPARATOR = '|';
+    public const RELATIVE_DATE_REGEX = '/^(today|week|month|year)(\|(\+|-)?\d+)?$/';
 
     /**
      * @var string
@@ -86,12 +95,75 @@ final class Range extends Token
     }
 
 
+    public function getStartDateValue(): ?DateTimeImmutable
+    {
+        if ($this->isStartInDateFormat()) {
+            return DateTimeImmutable::createFromFormat(self::DATE_FORMAT, $this->getStartValue());
+        }
+
+        return null;
+    }
+
+    public function getStartDateTimeValue(): ?DateTimeImmutable
+    {
+        if ($this->isStartInDateTimeFormat()) {
+            return DateTimeImmutable::createFromFormat(self::DATETIME_FORMAT, $this->getStartValue());
+        }
+
+        return null;
+    }
+
+    public function getStartRelativeDateValue(): ?array
+    {
+        if ($this->isStartInRelativeDateFormat()) {
+            $parts = explode(self::RELATIVE_DATE_SEPARATOR, $this->getStartValue());
+            $value = $parts[0];
+            $offset = isset($parts[1]) ? (int) $parts[1] : 0;
+
+            return ['value' => $value, 'offset' => $offset];
+        }
+
+        return null;
+    }
+
+
     /**
      * @return int|float|string
      */
     public function getEndValue()
     {
         return $this->endValue;
+    }
+
+    public function getEndDateValue(): ?DateTimeImmutable
+    {
+        if ($this->isEndInDateFormat()) {
+            return DateTimeImmutable::createFromFormat(self::DATE_FORMAT, $this->getEndValue());
+        }
+
+        return null;
+    }
+
+    public function getEndDateTimeValue(): ?DateTimeImmutable
+    {
+        if ($this->isEndInDateTimeFormat()) {
+            return DateTimeImmutable::createFromFormat(self::DATETIME_FORMAT, $this->getEndValue());
+        }
+
+        return null;
+    }
+
+    public function getEndRelativeDateValue(): ?array
+    {
+        if ($this->isEndInRelativeDateFormat()) {
+            $parts = explode(self::RELATIVE_DATE_SEPARATOR, $this->getEndValue());
+            $value = $parts[0];
+            $offset = isset($parts[1]) ? (int) $parts[1] : 0;
+
+            return ['value' => $value, 'offset' => $offset];
+        }
+
+        return null;
     }
 
 
@@ -146,6 +218,36 @@ final class Range extends Token
     public function getFlags(): ?Flags
     {
         return $this->flags;
+    }
+
+    public function isStartInDateFormat(): bool
+    {
+        return preg_match(self::DATE_REGEX, $this->getStartValue()) === 1;
+    }
+
+    public function isStartInDateTimeFormat(): bool
+    {
+        return preg_match(self::DATETIME_REGEX, $this->getStartValue()) === 1;
+    }
+
+    public function isStartInRelativeDateFormat(): bool
+    {
+        return preg_match(self::RELATIVE_DATE_REGEX, $this->getStartValue()) === 1;
+    }
+
+    public function isEndInDateFormat(): bool
+    {
+        return preg_match(self::DATE_REGEX, $this->getEndValue()) === 1;
+    }
+
+    public function isEndInDateTimeFormat(): bool
+    {
+        return preg_match(self::DATETIME_REGEX, $this->getEndValue()) === 1;
+    }
+
+    public function isEndInRelativeDateFormat(): bool
+    {
+        return preg_match(self::RELATIVE_DATE_REGEX, $this->getEndValue()) === 1;
     }
 
 
